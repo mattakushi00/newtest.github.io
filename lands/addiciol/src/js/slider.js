87408b81738f40dee9f2948b1ac6899af1d6801d
gromov_Max <m.gromov@leadtop.local>
2021-09-02 16:30:54 +0300

    addiciol - fix

--- lands/addiciol/src/js/slider.js
+++ lands/addiciol/src/js/slider.js
@@ -1,31 +1,35 @@
 import $ from 'jquery'
 import 'slick-carousel'
 
-$('.comments__list').slick({
-	infinite: true,
-	slidesToShow: 3,
-	slidesToScroll: 1,
-	adaptiveHeight: true,
-	responsive: [
-		{
-			breakpoint: 992,
-			settings: {
-				arrows: false,
-				dots: true,
-				slidesToShow: 2,
-				adaptiveHeight: true,
-			}
-		},
-		{
-			breakpoint: 769,
-			settings: {
-				arrows: false,
-				dots: true,
-				slidesToShow: 1,
-				adaptiveHeight: true,
-			}
-		},
-	]
-})
+slickFunc('.how__list')
+slickFunc('.comments__list')
 
+function slickFunc(domElement) {
+	$(domElement).slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		adaptiveHeight: true,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+					dots: true,
+					slidesToShow: 2,
+					adaptiveHeight: true,
+				}
+			},
+			{
+				breakpoint: 769,
+				settings: {
+					arrows: false,
+					dots: true,
+					slidesToShow: 1,
+					adaptiveHeight: true,
+				}
+			},
+		]
+	})
+}
 
